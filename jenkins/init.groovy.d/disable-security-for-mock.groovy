57f0ec08927c463f18f15f96d962f6b726a3554f
Tom PhongPhath <tom.pp.lpt@gmail.com>
2026-06-10 16:16:19 +0700

    Fix: graceful fail when branch not found + disable CSRF at system level

--- jenkins/init.groovy.d/disable-security-for-mock.groovy
+++ jenkins/init.groovy.d/disable-security-for-mock.groovy
@@ -1,10 +1,14 @@
-import hudson.security.AuthorizationStrategy
-import hudson.security.SecurityRealm
 import jenkins.model.Jenkins
+import hudson.security.SecurityRealm
+import hudson.security.AuthorizationStrategy
+import hudson.security.csrf.DefaultCrumbIssuer
 
 def jenkins = Jenkins.instance
+
 jenkins.setSecurityRealm(SecurityRealm.NO_AUTHENTICATION)
 jenkins.setAuthorizationStrategy(new AuthorizationStrategy.Unsecured())
+
+jenkins.setCrumbIssuer(null)
 jenkins.save()
 
-println("[disable-security-for-mock] Jenkins security disabled for local mock environment.")
+println("[disable-security-for-mock] Security disabled, CSRF disabled.")
